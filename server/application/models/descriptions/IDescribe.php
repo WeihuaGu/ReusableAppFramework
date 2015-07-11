3d7b6e462fb3c4b65bb718e9a0a4120940b74994
WeihuaGu <ministarmail@msn.cn>
2015-07-11 16:05:43 +0800

    copy from go the street.

---
+++ server/application/models/descriptions/IDescribe.php
@@ -0,0 +1,15 @@
+<?php
+/**
+描述系统借口
+
+
+@auther 
+*/
+interface IDescription{
+	public  function getDescription($object);
+	public  function addDescriptions($descriptions);
+	public  function delDescritpions($subdescritpions);
+	
+	
+}
+
